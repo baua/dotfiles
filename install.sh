5feb42c301ee504a2b1b01d713689b0059ce9377
Martin Lehmann <martin@ml-solutions.org>
2017-06-28 20:02:00 +1000

    Save progress

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@
 GITDIR="${HOME}/git"
 
 pkgbin="$(which apt-get)" || pkgbin="$(which yum)" || pkgbin="$(which zypper)"
+pkgmgr="${pkgbin##*/}"
 datestr=$(date +"%Y%m%d%H%M")
 logfile="/tmp/dotfiles.install.${datestr}"
 
@@ -14,18 +15,18 @@ ATOMGIT="https://github.com/nima/atomicles.git"
 echo "Starting install. See ${logfile} for more details."
 echo
 
-if [ ${#pgkbin[@]} -eq 0 ]; then
+if [ ${#pkgmgr} -eq 0 ]; then
     echo "No package manager found."
     exit 1
 fi
 
 declare -A packages
-packages['apt']="git wmii feh libxft-dev fonts-dejavu-core ttf-dejavu"
+packages['apt-get']="vim wmii feh libxft-dev fonts-dejavu-core ttf-dejavu"
 
 echo -n "Installing necessary packages ... "
-case "${pgkbin}" in
-    apt)
-        sudo apt-get install "${packages[@]}"
+case "${pkgmgr}" in
+    apt-get)
+        bash -c "sudo ${pkgbin} -y install ${packages[@]}"
         if [ $? -eq 0 ]; then
             echo " DONE."
             echo
@@ -36,18 +37,18 @@ case "${pgkbin}" in
         fi
         ;;
     *)
-        echo "Unsupported package manager (${pkgbin}).";
+        echo "Unsupported package manager (${pkgmgr}).";
         exit 1
 esac
 
 
 echo "Linking dot files  ... "
-local filename
-local bkpdir="${CWD}/tmp/dotfiles.backup.${datestr}"
+declare filename
+declare bkpdir="${PWD}/tmp/dotfiles.backup.${datestr}"
 for file in files/*; do
-    filename="files/${file}"
-    linkname="${HOME}/${file}"
-    if [ -e "${linkname}" ]; then
+    filename="${PWD}/${file}"
+    linkname="${HOME}/.${file##*/}"
+    if [ -f "${linkname}" ]; then
         mkdir -p "${bkpdir}"
         mv "${linkname}" "${bkpdir}"/
     fi
@@ -59,13 +60,50 @@ for file in files/*; do
     fi
 done
 
+[ ! -d "${GITDIR}" ]  && mkdir -p "${GITDIR}"
 pushd "${GITDIR}"
-echo "Getting wmiii from github  ... "
-git clone "${WMIIIGIT}" > "${logfile}" 2>&1
-if [ $? -eq 0 ]; then
-    echo " Done."
+echo -n "Getting atomicales from github  ... "
+[ ! -d "/opt/bin" ] && sudo mkdir -p /opt/bin
+if [ ! -d "atomicles" ]; then
+    git clone "${ATOMGIT}" > "${logfile}" 2>&1
+    if [ $? -eq 0 ]; then
+        echo " Done."
+        pushd atomicles
+        make
+        sudo make install
+    else
+        echo " Failed."
+        exit 1
+    fi
 else
-    echo " Failed."
-    exit 1
+    echo "directory atomicles already exists."
 fi
+
+
+echo -n "Getting wmiii from github  ... "
+if [ ! -d "wmiii" ]; then
+    git clone "${WMIIIGIT}" > "${logfile}" 2>&1
+    if [ $? -eq 0 ]; then
+        echo " Done."
+        if [ -d "${HOME}/.wmii" ]; then
+            "${HOME}/.wmii already exists. Relink or remove."
+        else
+            ln -sf "${PWD}/wmiii" "${HOME}/.wmii"
+            if [ $? -eq 0 ]; then
+                echo "Created ${HOME}/.wmii -> ${CWD}/wmiii."
+            else
+                echo "Link creation ${HOME}/.wmii -> ${CWD}/wmiii failed."
+            fi
+        fi
+    else
+        echo " Failed."
+        exit 1
+    fi
+else
+    echo "directory wmiii already exists."
+fi
+
+echo "Add the following line into /etc/fstab"
+echo "shm /dev/shm/wmii tmpfs defaults,user,noexec,nodev,nosuid,noauto,noatime,size=512M,nr_inodes=8k,mode=777 0 0"
+
 popd
